d07fe22e630ca3e04f593411d487d699cc0e3ce2
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-02 23:20:04 +0000

    final

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,9 @@
-const arr = process.argv.slice(2);
+//const arr = process.argv.slice(2);
 
 const request = require('request');
 
+
+/*
 request(`https://api.thecatapi.com/v1/breeds/search?q=${arr[0]}`, (error, response, body) => {
   
   if (error) {
@@ -15,9 +17,26 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${arr[0]}`, (error, respon
   } else {
     console.log("Sorry! Breed not found!");
   }
+
+}); */
+
+
+
+const fetchBreedDescription =  function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
-});
+    if (!error) {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        return callback("Error", null);
+      } else {
+        return callback(null, data[0]['description']);
+      }
+    } else {
+      return callback(error, null);
+    }
+  });
+};
 
-//module.exports = { fetchBreedDescription };
-
-//const breedDescription =  function ()
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Failed:', error);
+  } else {
+    console.log(desc);
+  }
+});
