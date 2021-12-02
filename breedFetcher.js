53bce69738ed7c364968f7b49b3c2270a4b0d2e7
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-02 20:28:58 +0000

    changes for failed case

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,13 +4,20 @@ const request = require('request');
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${arr[0]}`, (error, response, body) => {
   
-  const data = JSON.parse(body);
   if (error) {
-    console.log("Failed to access server");
+    console.log("Failed");
   }
+
+  const data = JSON.parse(body);
+
   if (Object.values(data).includes(data[0])) {
     console.log(`${data[0]['description']}`);
   } else {
     console.log("Sorry! Breed not found!");
   }
+  
 });
+
+//module.exports = { fetchBreedDescription };
+
+//const breedDescription =  function ()
